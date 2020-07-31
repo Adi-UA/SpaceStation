8eee3e10a6a5084e8c691de95730ecc73654457c
adi-ua <abanerjee@email.arizona.edu>
2020-07-31 12:49:57 -0700

    Single collision

--- polite_invaders/enemy_ship.py
+++ polite_invaders/enemy_ship.py
@@ -6,28 +6,34 @@ class enemyShip:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.y_velocity = 0.3/100*WIN_HEIGHT
+        self.has_collided = False
+        self.y_velocity = 0.27/100*WIN_HEIGHT
         self.img = ENEMY_SHIP_IMG_1
 
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
 
     def collide(self, player_ship):
-        player_ship_mask = player_ship.get_mask()
-        self_mask = pygame.mask.from_surface(self.img)
+        if not self.has_collided:
+            player_ship_mask = player_ship.get_mask()
+            self_mask = pygame.mask.from_surface(self.img)
 
-        offset = (round(self.x - player_ship.x), round(self.y - player_ship.y))
+            offset = (round(self.x - player_ship.x), round(self.y - player_ship.y))
 
-        overlaps = player_ship_mask.overlap(self_mask, offset)
+            overlaps = player_ship_mask.overlap(self_mask, offset)
 
-        if overlaps:
-            return True
+            if overlaps:
+                return True
+                self.has_collided = True
+            else:
+                return False
         else:
             return False
 
     def move(self, reverse=False):
         if reverse:
-            self.y_velocity = -self.y_velocity
+            self.y = self.y - 32
+            self.y_velocity = -self.y_velocity*2
 
         self.y += self.y_velocity
 
@@ -36,6 +42,7 @@ class enemyShipCreeper(enemyShip):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.img = ENEMY_SHIP_IMG_1
+
 
 class enemyShipDeathStar(enemyShip):
     def __init__(self, x, y):
