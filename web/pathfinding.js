7adf7035d09c447f23ab9599b711cf3241bd223e
dev-ved30 <vedshah.email@gmail.com>
2020-08-01 22:27:31 +0530

    Path Finder now uses rick and morty theme

--- web/pathfinding.js
+++ web/pathfinding.js
@@ -20,7 +20,7 @@ var spritesheet = null;
 var spritesheetLoaded = false;
 
 // the world grid: a 2d array of tiles
-var world = [[]];
+var world = [ [] ];
 
 // size in the world in sprite tiles
 var worldWidth = 16;
@@ -31,26 +31,25 @@ var tileWidth = 32;
 var tileHeight = 32;
 
 // start and end of path
-var pathStart = [worldWidth,worldHeight];
-var pathEnd = [0,0];
+var pathStart = [ worldWidth, worldHeight ];
+var pathEnd = [ 0, 0 ];
 var currentPath = [];
 
 // ensure that concole.log doesn't cause errors
-if (typeof console == "undefined") var console = { log: function() {} };
+if (typeof console == 'undefined') var console = { log: function() {} };
 
 // the html page is ready
-function onload()
-{
+function onload() {
 	console.log('Page loaded.');
 	canvas = document.getElementById('gameCanvas');
 	canvas.width = worldWidth * tileWidth;
 	canvas.height = worldHeight * tileHeight;
-	canvas.addEventListener("click", canvasClick, false);
+	canvas.addEventListener('click', canvasClick, false);
 	if (!canvas) alert('Blah!');
-	ctx = canvas.getContext("2d");
+	ctx = canvas.getContext('2d');
 	if (!ctx) alert('Hmm!');
 	spritesheet = new Image();
-	spritesheet.src = 'spritesheet.png';
+	spritesheet.src = 'pix.png';
 	// the image above has been turned into a data url
 	// so that no external files are required for
 	// this web page - useful for included in a
@@ -60,55 +59,44 @@ function onload()
 }
 
 // the spritesheet is ready
-function loaded()
-{
+function loaded() {
 	console.log('Spritesheet loaded.');
 	spritesheetLoaded = true;
 	createWorld();
 }
 
 // fill the world with walls
-function createWorld()
-{
+function createWorld() {
 	console.log('Creating world...');
 
 	// create emptiness
-	for (var x=0; x < worldWidth; x++)
-	{
+	for (var x = 0; x < worldWidth; x++) {
 		world[x] = [];
 
-		for (var y=0; y < worldHeight; y++)
-		{
+		for (var y = 0; y < worldHeight; y++) {
 			world[x][y] = 0;
 		}
 	}
 
 	// scatter some walls
-	for (var x=0; x < worldWidth; x++)
-	{
-		for (var y=0; y < worldHeight; y++)
-		{
-			if (Math.random() > 0.75)
-			world[x][y] = 1;
+	for (var x = 0; x < worldWidth; x++) {
+		for (var y = 0; y < worldHeight; y++) {
+			if (Math.random() > 0.75) world[x][y] = 1;
 		}
 	}
 
 	// calculate initial possible path
 	// note: unlikely but possible to never find one...
 	currentPath = [];
-	while (currentPath.length == 0)
-	{
-		pathStart = [Math.floor(Math.random()*worldWidth),Math.floor(Math.random()*worldHeight)];
-		pathEnd = [Math.floor(Math.random()*worldWidth),Math.floor(Math.random()*worldHeight)];
-		if (world[pathStart[0]][pathStart[1]] == 0)
-		currentPath = findPath(world,pathStart,pathEnd);
+	while (currentPath.length == 0) {
+		pathStart = [ Math.floor(Math.random() * worldWidth), Math.floor(Math.random() * worldHeight) ];
+		pathEnd = [ Math.floor(Math.random() * worldWidth), Math.floor(Math.random() * worldHeight) ];
+		if (world[pathStart[0]][pathStart[1]] == 0) currentPath = findPath(world, pathStart, pathEnd);
 	}
 	redraw();
-
 }
 
-function redraw()
-{
+function redraw() {
 	if (!spritesheetLoaded) return;
 
 	console.log('redrawing...');
@@ -119,77 +107,75 @@ function redraw()
 	ctx.fillStyle = '#000000';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	for (var x=0; x < worldWidth; x++)
-	{
-		for (var y=0; y < worldHeight; y++)
-		{
-
+	for (var x = 0; x < worldWidth; x++) {
+		for (var y = 0; y < worldHeight; y++) {
 			// choose a sprite to draw
-			switch(world[x][y])
-			{
-			case 1:
-				spriteNum = 1;
-				break;
-			default:
-				spriteNum = 0;
-				break;
+			switch (world[x][y]) {
+				case 1:
+					spriteNum = 1;
+					break;
+				default:
+					spriteNum = 0;
+					break;
 			}
 
 			// draw it
 			// ctx.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
-			ctx.drawImage(spritesheet,
-			spriteNum*tileWidth, 0,
-			tileWidth, tileHeight,
-			x*tileWidth, y*tileHeight,
-			tileWidth, tileHeight);
-
+			ctx.drawImage(
+				spritesheet,
+				spriteNum * tileWidth,
+				0,
+				tileWidth,
+				tileHeight,
+				x * tileWidth,
+				y * tileHeight,
+				tileWidth,
+				tileHeight
+			);
 		}
 	}
 
 	// draw the path
-	console.log('Current path length: '+currentPath.length);
-	for (rp=0; rp<currentPath.length; rp++)
-	{
-		switch(rp)
-		{
-		case 0:
-			spriteNum = 2; // start
-			break;
-		case currentPath.length-1:
-			spriteNum = 3; // end
-			break;
-		default:
-			spriteNum = 4; // path node
-			break;
+	console.log('Current path length: ' + currentPath.length);
+	for (rp = 0; rp < currentPath.length; rp++) {
+		switch (rp) {
+			case 0:
+				spriteNum = 2; // start
+				break;
+			case currentPath.length - 1:
+				spriteNum = 3; // end
+				break;
+			default:
+				spriteNum = 4; // path node
+				break;
 		}
 
-		ctx.drawImage(spritesheet,
-		spriteNum*tileWidth, 0,
-		tileWidth, tileHeight,
-		currentPath[rp][0]*tileWidth,
-		currentPath[rp][1]*tileHeight,
-		tileWidth, tileHeight);
-	}		
+		ctx.drawImage(
+			spritesheet,
+			spriteNum * tileWidth,
+			0,
+			tileWidth,
+			tileHeight,
+			currentPath[rp][0] * tileWidth,
+			currentPath[rp][1] * tileHeight,
+			tileWidth,
+			tileHeight
+		);
+	}
 }
 
 // handle click events on the canvas
-function canvasClick(e)
-{
+function canvasClick(e) {
 	var x;
 	var y;
 
 	// grab html page coords
-	if (e.pageX != undefined && e.pageY != undefined)
-	{
+	if (e.pageX != undefined && e.pageY != undefined) {
 		x = e.pageX;
 		y = e.pageY;
-	}
-	else
-	{
-		x = e.clientX + document.body.scrollLeft +
-		document.documentElement.scrollLeft;
-		y = e.clientY + document.body.scrollTop +
-		document.documentElement.scrollTop;
+	} else {
+		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 	}
 
 	// make them relative to the canvas only
@@ -197,32 +183,27 @@ function canvasClick(e)
 	y -= canvas.offsetTop;
 
 	// return tile x,y that we clicked
-	var cell =
-	[
-	Math.floor(x/tileWidth),
-	Math.floor(y/tileHeight)
-	];
+	var cell = [ Math.floor(x / tileWidth), Math.floor(y / tileHeight) ];
 
 	// now we know while tile we clicked
-	console.log('we clicked tile '+cell[0]+','+cell[1]);
+	console.log('we clicked tile ' + cell[0] + ',' + cell[1]);
 
 	pathStart = pathEnd;
 	pathEnd = cell;
 
 	// calculate path
-	currentPath = findPath(world,pathStart,pathEnd);
+	currentPath = findPath(world, pathStart, pathEnd);
 	redraw();
 }
 
 // world is a 2d array of integers (eg world[10][15] = 0)
 // pathStart and pathEnd are arrays like [5,10]
-function findPath(world, pathStart, pathEnd)
-{
+function findPath(world, pathStart, pathEnd) {
 	// shortcuts for speed
-	var	abs = Math.abs;
-	var	max = Math.max;
-	var	pow = Math.pow;
-	var	sqrt = Math.sqrt;
+	var abs = Math.abs;
+	var max = Math.max;
+	var pow = Math.pow;
+	var sqrt = Math.sqrt;
 
 	// the world data are integers:
 	// anything higher than this number is considered blocked
@@ -231,17 +212,17 @@ function findPath(world, pathStart, pathEnd)
 	var maxWalkableTileNum = 0;
 
 	// keep track of the world dimensions
-    // Note that this A-star implementation expects the world array to be square: 
-	// it must have equal height and width. If your game world is rectangular, 
+	// Note that this A-star implementation expects the world array to be square:
+	// it must have equal height and width. If your game world is rectangular,
 	// just fill the array with dummy values to pad the empty space.
 	var worldWidth = world[0].length;
 	var worldHeight = world.length;
-	var worldSize =	worldWidth * worldHeight;
+	var worldSize = worldWidth * worldHeight;
 
 	// which heuristic should we use?
 	// default: no diagonals (Manhattan)
 	var distanceFunction = ManhattanDistance;
-	var findNeighbours = function(){}; // empty
+	var findNeighbours = function() {}; // empty
 
 	/*
 
@@ -268,18 +249,18 @@ function findPath(world, pathStart, pathEnd)
 	// distanceFunction functions
 	// these return how far away a point is to another
 
-	function ManhattanDistance(Point, Goal)
-	{	// linear movement - no diagonals - just cardinal directions (NSEW)
+	function ManhattanDistance(Point, Goal) {
+		// linear movement - no diagonals - just cardinal directions (NSEW)
 		return abs(Point.x - Goal.x) + abs(Point.y - Goal.y);
 	}
 
-	function DiagonalDistance(Point, Goal)
-	{	// diagonal movement - assumes diag dist is 1, same as cardinals
+	function DiagonalDistance(Point, Goal) {
+		// diagonal movement - assumes diag dist is 1, same as cardinals
 		return max(abs(Point.x - Goal.x), abs(Point.y - Goal.y));
 	}
 
-	function EuclideanDistance(Point, Goal)
-	{	// diagonals are considered a little farther than cardinal directions
+	function EuclideanDistance(Point, Goal) {
+		// diagonals are considered a little farther than cardinal directions
 		// diagonal movement using Euclide (AC = sqrt(AB^2 + BC^2))
 		// where AB = x2 - x1 and BC = y2 - y1 and AC will be [x3, y3]
 		return sqrt(pow(Point.x - Goal.x, 2) + pow(Point.y - Goal.y, 2));
@@ -291,25 +272,20 @@ function findPath(world, pathStart, pathEnd)
 	// Returns every available North, South, East or West
 	// cell that is empty. No diagonals,
 	// unless distanceFunction function is not Manhattan
-	function Neighbours(x, y)
-	{
-		var	N = y - 1,
-		S = y + 1,
-		E = x + 1,
-		W = x - 1,
-		myN = N > -1 && canWalkHere(x, N),
-		myS = S < worldHeight && canWalkHere(x, S),
-		myE = E < worldWidth && canWalkHere(E, y),
-		myW = W > -1 && canWalkHere(W, y),
-		result = [];
-		if(myN)
-		result.push({x:x, y:N});
-		if(myE)
-		result.push({x:E, y:y});
-		if(myS)
-		result.push({x:x, y:S});
-		if(myW)
-		result.push({x:W, y:y});
+	function Neighbours(x, y) {
+		var N = y - 1,
+			S = y + 1,
+			E = x + 1,
+			W = x - 1,
+			myN = N > -1 && canWalkHere(x, N),
+			myS = S < worldHeight && canWalkHere(x, S),
+			myE = E < worldWidth && canWalkHere(E, y),
+			myW = W > -1 && canWalkHere(W, y),
+			result = [];
+		if (myN) result.push({ x: x, y: N });
+		if (myE) result.push({ x: E, y: y });
+		if (myS) result.push({ x: x, y: S });
+		if (myW) result.push({ x: W, y: y });
 		findNeighbours(myN, myS, myE, myW, N, S, E, W, result);
 		return result;
 	}
@@ -317,90 +293,71 @@ function findPath(world, pathStart, pathEnd)
 	// returns every available North East, South East,
 	// South West or North West cell - no squeezing through
 	// "cracks" between two diagonals
-	function DiagonalNeighbours(myN, myS, myE, myW, N, S, E, W, result)
-	{
-		if(myN)
-		{
-			if(myE && canWalkHere(E, N))
-			result.push({x:E, y:N});
-			if(myW && canWalkHere(W, N))
-			result.push({x:W, y:N});
+	function DiagonalNeighbours(myN, myS, myE, myW, N, S, E, W, result) {
+		if (myN) {
+			if (myE && canWalkHere(E, N)) result.push({ x: E, y: N });
+			if (myW && canWalkHere(W, N)) result.push({ x: W, y: N });
 		}
-		if(myS)
-		{
-			if(myE && canWalkHere(E, S))
-			result.push({x:E, y:S});
-			if(myW && canWalkHere(W, S))
-			result.push({x:W, y:S});
+		if (myS) {
+			if (myE && canWalkHere(E, S)) result.push({ x: E, y: S });
+			if (myW && canWalkHere(W, S)) result.push({ x: W, y: S });
 		}
 	}
 
 	// returns every available North East, South East,
 	// South West or North West cell including the times that
 	// you would be squeezing through a "crack"
-	function DiagonalNeighboursFree(myN, myS, myE, myW, N, S, E, W, result)
-	{
+	function DiagonalNeighboursFree(myN, myS, myE, myW, N, S, E, W, result) {
 		myN = N > -1;
 		myS = S < worldHeight;
 		myE = E < worldWidth;
 		myW = W > -1;
-		if(myE)
-		{
-			if(myN && canWalkHere(E, N))
-			result.push({x:E, y:N});
-			if(myS && canWalkHere(E, S))
-			result.push({x:E, y:S});
+		if (myE) {
+			if (myN && canWalkHere(E, N)) result.push({ x: E, y: N });
+			if (myS && canWalkHere(E, S)) result.push({ x: E, y: S });
 		}
-		if(myW)
-		{
-			if(myN && canWalkHere(W, N))
-			result.push({x:W, y:N});
-			if(myS && canWalkHere(W, S))
-			result.push({x:W, y:S});
+		if (myW) {
+			if (myN && canWalkHere(W, N)) result.push({ x: W, y: N });
+			if (myS && canWalkHere(W, S)) result.push({ x: W, y: S });
 		}
 	}
 
 	// returns boolean value (world cell is available and open)
-	function canWalkHere(x, y)
-	{
-		return ((world[x] != null) &&
-			(world[x][y] != null) &&
-			(world[x][y] <= maxWalkableTileNum));
-	};
+	function canWalkHere(x, y) {
+		return world[x] != null && world[x][y] != null && world[x][y] <= maxWalkableTileNum;
+	}
 
 	// Node function, returns a new object with Node properties
 	// Used in the calculatePath function to store route costs, etc.
-	function Node(Parent, Point)
-	{
+	function Node(Parent, Point) {
 		var newNode = {
 			// pointer to another Node object
-			Parent:Parent,
+			Parent: Parent,
 			// array index of this Node in the world linear array
-			value:Point.x + (Point.y * worldWidth),
+			value: Point.x + Point.y * worldWidth,
 			// the location coordinates of this Node
-			x:Point.x,
-			y:Point.y,
+			x: Point.x,
+			y: Point.y,
 			// the heuristic estimated cost
 			// of an entire path using this node
-			f:0,
+			f: 0,
 			// the distanceFunction cost to get
 			// from the starting point to this node
-			g:0
+			g: 0
 		};
 
 		return newNode;
 	}
 
 	// Path function, executes AStar algorithm operations
-	function calculatePath()
-	{
+	function calculatePath() {
 		// create Nodes from the Start and End x,y coordinates
-		var	mypathStart = Node(null, {x:pathStart[0], y:pathStart[1]});
-		var mypathEnd = Node(null, {x:pathEnd[0], y:pathEnd[1]});
+		var mypathStart = Node(null, { x: pathStart[0], y: pathStart[1] });
+		var mypathEnd = Node(null, { x: pathEnd[0], y: pathEnd[1] });
 		// create an array that will contain all world cells
 		var AStar = new Array(worldSize);
 		// list of currently open Nodes
-		var Open = [mypathStart];
+		var Open = [ mypathStart ];
 		// list of closed Nodes
 		var Closed = [];
 		// list of the final output array
@@ -414,14 +371,11 @@ function findPath(world, pathStart, pathEnd)
 		// temp integer variables used in the calculations
 		var length, max, min, i, j;
 		// iterate through the open list until none are left
-		while(length = Open.length)
-		{
+		while ((length = Open.length)) {
 			max = worldSize;
 			min = -1;
-			for(i = 0; i < length; i++)
-			{
-				if(Open[i].f < max)
-				{
+			for (i = 0; i < length; i++) {
+				if (Open[i].f < max) {
 					max = Open[i].f;
 					min = i;
 				}
@@ -429,29 +383,23 @@ function findPath(world, pathStart, pathEnd)
 			// grab the next node and remove it from Open array
 			myNode = Open.splice(min, 1)[0];
 			// is it the destination node?
-			if(myNode.value === mypathEnd.value)
-			{
+			if (myNode.value === mypathEnd.value) {
 				myPath = Closed[Closed.push(myNode) - 1];
-				do
-				{
-					result.push([myPath.x, myPath.y]);
-				}
-				while (myPath = myPath.Parent);
+				do {
+					result.push([ myPath.x, myPath.y ]);
+				} while ((myPath = myPath.Parent));
 				// clear the working arrays
 				AStar = Closed = Open = [];
 				// we want to return start to finish
 				result.reverse();
-			}
-			else // not the destination
-			{
+			} else {
+				// not the destination
 				// find which nearby nodes are walkable
 				myNeighbours = Neighbours(myNode.x, myNode.y);
 				// test each one that hasn't been tried already
-				for(i = 0, j = myNeighbours.length; i < j; i++)
-				{
+				for (i = 0, j = myNeighbours.length; i < j; i++) {
 					myPath = Node(myNode, myNeighbours[i]);
-					if (!AStar[myPath.value])
-					{
+					if (!AStar[myPath.value]) {
 						// estimated cost of this particular route so far
 						myPath.g = myNode.g + distanceFunction(myNeighbours[i], myNode);
 						// estimated cost of entire guessed route to the destination
@@ -473,6 +421,4 @@ function findPath(world, pathStart, pathEnd)
 	// this returns an array of coordinates
 	// that is empty if no path is possible
 	return calculatePath();
-
 } // end of findPath() function
-
