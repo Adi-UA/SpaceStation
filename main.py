0d7b443fe57a43435e4c548aa94aa6faa999009d
adi-ua <abanerjee@email.arizona.edu>
2020-08-01 13:15:35 -0700

    Windows suppirt??

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import eel
 import subprocess
+import sys
 
 @eel.expose
 def play_game():
-    try:
-        subprocess.run(["python3", "polite_invaders/play.py"])
-    except:
+    if (sys.platform).startswith("win"):
         subprocess.run(["python", "polite_invaders"+ "\\" + "play.py"])
+    else:
+        subprocess.run(["python3", "polite_invaders/play.py"])
+       
 
 @eel.expose
 def train_ai():
-    try:
+    if (sys.platform).startswith("win"):
+        subprocess.run(["python", "polite_invaders"+ "\\"+ "trainAI.py"])    
+    else:
         subprocess.run(["python3", "polite_invaders/trainAI.py"])
-    except:
-        subprocess.run(["python", "polite_invaders"+ "\\"+ "trainAI.py"])
+        
 
 @eel.expose
 def test_ai():
-    try:
+    if (sys.platform).startswith("win"):
+        subprocess.run(["python", "polite_invaders" + "\\" + "testAI.py"])    
+    else:
         subprocess.run(["python3", "polite_invaders/testAI.py"])
-    except:
-        subprocess.run(["python", "polite_invaders" + "\\" + "testAI.py"])
+        
 
 if __name__ == "__main__":
     eel.init("web")
